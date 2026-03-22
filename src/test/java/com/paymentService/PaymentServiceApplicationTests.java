55d028594827e2a415d8ac6efcc666226fceea36
hirun athukorala <hirunbhathiya@gmail.com>
2026-03-22 22:00:00 +0530

    kafka removed

--- src/test/java/com/paymentService/PaymentServiceApplicationTests.java
+++ src/test/java/com/paymentService/PaymentServiceApplicationTests.java
@@ -2,16 +2,11 @@ package com.paymentService;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class PaymentServiceApplicationTests {
-
-	@MockitoBean
-	private KafkaTemplate<String, String> kafkaTemplate;
 
 	@Test
 	void contextLoads() {
